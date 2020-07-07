b3af2bb57f10a8b95a0f86c44836f3c088e6024a
Sergejs Koreskovs <skoreskovs@evolutiongaming.com>
2020-07-07 11:56:15 +0300

    123

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -5,6 +5,7 @@ describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
+
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
