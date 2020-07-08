87e6c46a0cd7803651923f67d691a8ad64a7f658
Sergejs Koreskovs <skoreskovs@evolutiongaming.com>
2020-07-08 14:04:43 +0300

    DEPLOY

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,20 +1,26 @@
 <template>
     <div>
-      <v-list-item>
-      <v-list-item-avatar>
-          <v-icon :class="`${color}--text`">{{ icon }}</v-icon>
-      </v-list-item-avatar>
-      <v-list-item-content>
-          <v-list-item-subtitle>{{ name }}</v-list-item-subtitle>
-          <v-list-item-title>{{total.toFixed(2)}}$</v-list-item-title>
-      </v-list-item-content>
-      <v-spacer></v-spacer>
-      <v-list-item-action>
-          <v-btn depressed :class="`${color}--text`">
-            <v-icon>mdi-plus</v-icon>
-          </v-btn>
-      </v-list-item-action>
-      </v-list-item>
+      <v-hover
+        v-slot:default="{ hover }"
+      >
+      <v-card :elevation="hover ? 16 : 0">
+        <v-list-item>
+        <v-list-item-avatar>
+            <v-icon :class="`${color}--text`">{{ icon }}</v-icon>
+        </v-list-item-avatar>
+        <v-list-item-content>
+            <v-list-item-subtitle>{{ name }}</v-list-item-subtitle>
+            <v-list-item-title>{{total.toFixed(2)}}$</v-list-item-title>
+        </v-list-item-content>
+        <v-spacer></v-spacer>
+        <v-list-item-action>
+            <v-btn depressed :class="`${color}--text`">
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+        </v-list-item-action>
+        </v-list-item>
+      </v-card>
+      </v-hover>
     </div>
 </template>
 
