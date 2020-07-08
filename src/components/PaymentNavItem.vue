f2fade1ca561db5fa0e1c9a25e1ae6f564f9755f
Sergejs Koreskovs <skoreskovs@evolutiongaming.com>
2020-07-08 14:18:59 +0300

    DEPLOY

--- src/components/PaymentNavItem.vue
+++ src/components/PaymentNavItem.vue
@@ -1,13 +1,17 @@
 <template>
-    <v-list-item two-line>
-        <v-list-item-avatar>
-            <v-icon :class="`${color}--text`">{{ icon }}</v-icon>
-        </v-list-item-avatar>
-        <v-list-item-content>
-            <v-list-item-subtitle>{{ name }}</v-list-item-subtitle>
-            <v-list-item-title>{{ total.toFixed(2) }}$</v-list-item-title>
-        </v-list-item-content>
-    </v-list-item>
+    <v-hover
+      v-slot:default="{ hover }"
+    >
+        <v-list-item two-line>
+            <v-list-item-avatar>
+                <v-icon :class="`${hover ? 'text--darken-2 blue' : color}--text`">{{ icon }}</v-icon>
+            </v-list-item-avatar>
+            <v-list-item-content>
+                <v-list-item-subtitle>{{ name }}</v-list-item-subtitle>
+                <v-list-item-title>{{ total.toFixed(2) }}$</v-list-item-title>
+            </v-list-item-content>
+        </v-list-item>
+    </v-hover>
 </template>
 
 <script>
