815ec64aa4faca557485ecc8a3286012b6b769e7
Sergejs Koreskovs <skoreskovs@evolutiongaming.com>
2020-08-18 15:06:34 +0300

    DEPLOY

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -2,7 +2,10 @@
     <div class="keyboard">
         <v-row>
             <v-col cols="3" v-for="btnItem in buttons" :key="btnItem.text">
-                <v-btn text class="w100">{{ btnItem.text }}</v-btn>
+                <v-btn v-show="btnItem.text" text class="w100">{{ btnItem.text }}</v-btn>
+                <v-btn v-show="btnItem.icon" text class="w100">
+                  <v-icon>{{ btnItem.icon }}</v-icon>
+                </v-btn>
             </v-col>
         </v-row>
     </div>
@@ -11,25 +14,37 @@
 <script>
 export default {
   name: 'Keyboard',
+  methods: {
+    add (char) {
+      return () => (this.result += char)
+    },
+    remove () {
+      return this.result.slice(0, -1)
+    },
+    calc () {
+
+    }
+  },
   data () {
     return {
+      result: '',
       buttons: [
-        { text: 7, icon: false },
-        { text: 8, icon: false },
-        { text: 9, icon: false },
-        { text: '', icon: false },
-        { text: 4, icon: false },
-        { text: 5, icon: false },
-        { text: 6, icon: false },
-        { text: '', icon: false },
-        { text: 1, icon: false },
-        { text: 2, icon: false },
-        { text: 3, icon: false },
-        { text: '', icon: false },
-        { text: '', icon: false },
-        { text: 0, icon: false },
-        { text: '', icon: false },
-        { text: '', icon: false }
+        { text: '7', action: this.add('7') },
+        { text: '8', action: this.add('8') },
+        { text: '9', action: this.add('9') },
+        { text: '-', action: this.add('-') },
+        { text: '4', action: this.add('4') },
+        { text: '5', action: this.add('5') },
+        { text: '6', action: this.add('6') },
+        { text: '+', action: this.add('+') },
+        { text: '1', action: this.add('1') },
+        { text: '2', action: this.add('2') },
+        { text: '3', action: this.add('3') },
+        { text: '=', action: this.calc },
+        { text: '.', action: this.add('.') },
+        { text: '0', action: this.add('0') },
+        { icon: 'mdi-backspace-outline', action: this.remove },
+        { icon: 'mdi-check-circle-outline', action: this.save }
       ]
     }
   }
